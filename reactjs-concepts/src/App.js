5a5a7440113706d6baafab57dcecb791ca11d161
Sandro Mai <sandromai6@gmail.com>
2020-10-30 18:29:45 -0300

    Challenge Concluded

---
+++ reactjs-concepts/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: `New repository ${Date.now()}`,
+      url: 'https://www.github.com/sandromai',
+      techs: [
+        'Node.js',
+        'React JS'
+      ]
+    });
+
+    setRepositories([...repositories, response.data]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
